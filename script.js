7796a710e87ba4d1591585d24e3ccfca53d4038c
Suhas Bharadwaj <suhas.five@gmail.com>
2015-05-25 00:25:53 +0530

    Proper Font Sizes
    Adjusted the font sizes back to their proper versions and created the
    first commit.

---
+++ script.js
@@ -0,0 +1,11 @@
+function bold() {
+  document.execCommand('bold');
+}
+
+function italic() {
+  document.execCommand('italic');
+}
+
+function underline() {
+  document.execCommand('underline');
+}
