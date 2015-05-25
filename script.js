4bac1456f2e40000f9994dd6ae45074addb56f6c
Suhas Bharadwaj <suhas.five@gmail.com>
2015-05-26 00:24:06 +0530

    Fitting the content in.
    Added increasing the size of the whitespace for writing as the number of
    lines increases.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var writingarea = document.getElementById('writing_content');
+var ctrlDown = false;
+
 function bold() {
   document.execCommand('bold');
 }
@@ -9,3 +12,29 @@ function italic() {
 function underline() {
   document.execCommand('underline');
 }
+
+var a = 0;
+
+function loaded(e) {
+  if (e.which == 13) {
+    //alert("returned");
+    a++;
+  }
+  if (e.which == 17) {
+    ctrlDown = true;
+  }
+  if(ctrlDown && e.which == 86) {
+    alert("Pasted!");
+    ctrlDown = false;
+  }
+  if (a == 12) {
+    alert("End of the line;");
+    
+  }
+  console.log(e.keyCode);
+}
+//writing.on('keypress', function(e){console.log(e);});
+
+
+
+//$('#writing_content').bind("keyup", function(e) {console.log(String.fromCharCode(e.keyCode));});
